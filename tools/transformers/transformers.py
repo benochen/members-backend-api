22e76f643c8332341ab81c5038e5a1ad6ca4b46c
benoit <ben57univ@gmail.com>
2023-11-03 09:52:55 +0100

    Correct transformers

--- tools/transformers/transformers.py
+++ tools/transformers/transformers.py
@@ -7,5 +7,20 @@ class Transformers:
     @staticmethod
     def transform_document_list_to_dict(document_list):
         logger.debug("transformer")
+        members=list()
         for document in document_list:
+            member=dict()
             logger.debug(document.first_name)
+            member.last_name=document.last_name
+            member.first_name=document.first_name
+            member.phone=document.phone
+            member.mail=document.email
+            member.adresse=document.address
+            member.ville=document.ville
+            member.zip=document.zip_code
+            member.authorized_rs=document.social_network_authorized
+            member.authorized_web=document.ecreadys_site_web_authorized
+            member.authorized_press=document.press_media_authorized
+            member.cotisation=document.payment
+            members.append(member)
+        return members
